631d4454ad34ea57309d021ffd45a71b03ebe5b4
xt01102058 <moca_tao7@foxmail.com>
2021-10-13 17:08:31 +0800

    fix: to fix install error

--- test/config.js
+++ test/config.js
@@ -17,4 +17,4 @@ config.sts = {
   endpoint: env.ONCI ? 'https://sts.aliyuncs.com/' : null
 };
 
-config.metaSyncTime = env.ONCI ? '20s' : '1000ms';
+config.metaSyncTime = env.ONCI ? '30s' : '1000ms';
